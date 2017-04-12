fd00cb1e4d768c5bc452c6fd26b079f4e76fb499
freddyflo <fred.aklamanu@gmail.com>
2017-04-12 21:45:12 +0200

    DAO and Entity Implemented

---
+++ web-customer-tracker/src/com/freddyflo/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.freddyflo.springdemo.dao;
+
+import java.util.List;
+
+import com.freddyflo.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
